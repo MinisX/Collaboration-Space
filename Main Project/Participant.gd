bc684e83756db673cae93c0de2b020f4b13029d5
MinisX <daniel.dedoukh@gmail.com>
2022-10-03 17:35:13 +0200

    git setup

---
+++ Main Project/Participant.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+var velocity: Vector2 = Vector2()
+export (float) var speed = 100.0 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+
+func _process(delta):
+	get_input()
+	velocity = move_and_slide(velocity)
