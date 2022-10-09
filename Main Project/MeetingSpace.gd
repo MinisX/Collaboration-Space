03db28fc36a102bb2eadc60bd065ac73513473c4
fsk <fsk>
2022-10-09 21:15:28 +0200

    Updated customize avatar logic

--- Main Project/MeetingSpace.gd
+++ Main Project/MeetingSpace.gd
@@ -3,4 +3,4 @@ extends Node2D
 onready var particpant: KinematicBody2D = $YSort/Participant
 
 func _ready() -> void:
-	particpant.set_participant_name(Meeting.participant_name)
+	particpant.init(Meeting.participant_data)
