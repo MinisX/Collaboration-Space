6cf63702349d850c40129724a89b5bdc0b5156a3
fsk <fsk>
2022-10-09 21:14:15 +0200

    Added customize avatar UI and logic

--- Main Project/EnteringScene.gd
+++ Main Project/EnteringScene.gd
@@ -1,21 +1,40 @@
 extends Control
 
 # instantiate Member variables
-onready var join_button: Button = $MarginContainer/VBoxContainer/JoinButton
+onready var join_button: Button = $MarginContainer/VBoxContainer/HBoxContainer2/JoinButton
+onready var customize_button: Button = $MarginContainer/VBoxContainer/HBoxContainer2/CustomizeButton
 onready var LoginButton: Button = $MarginContainer/VBoxContainer/HBoxContainer/LoginButton
 onready var RegisterButton: Button = $MarginContainer/VBoxContainer/HBoxContainer/RegisterButton
 onready var participant_name_input: LineEdit = $MarginContainer/VBoxContainer/ParticipantName
+onready var back_button: Button = $CustomizeUI_Container/HBoxContainer/BackButton
+onready var color_picker: ColorPicker = $CustomizeUI_Container/HBoxContainer/ColorPicker
+onready var avatar = $CustomizeUI_Container/HBoxContainer/Avatar
 
 func _ready() -> void:
 	# Connect buttons to suitable callbacks
-	join_button.connect("pressed", self, "_on_join_pressed")		
+	join_button.connect("pressed", self, "_on_join_pressed")
+	customize_button.connect("pressed", self, "_on_customize_pressed")
 	LoginButton.connect("pressed", self, "_on_LoginButton_pressed")
 	RegisterButton.connect("pressed", self, "_on_RegisterButton_pressed")
+	back_button.connect("pressed", self, "_on_back_button_pressed")
+	color_picker.connect("color_changed", self, "_on_color_value_changed")
 
 # Called when join button is pressed
 func _on_join_pressed() -> void:
-	Meeting.participant_name = participant_name_input.text
+	Meeting.participant_data["name"] = participant_name_input.text
 	get_tree().change_scene("res://MeetingSpace.tscn")
+	
+func _on_customize_pressed() -> void:
+	$MarginContainer.hide()
+	$CustomizeUI_Container.show()
+
+func _on_color_value_changed(color) -> void:
+	Meeting.participant_data["color"] = color
+	avatar.set_body_color(color)
+
+func _on_back_button_pressed() -> void:
+	$MarginContainer.show()
+	$CustomizeUI_Container.hide()
 
 # Called when login button is pressed
 func _on_LoginButton_pressed():
@@ -24,3 +43,4 @@ func _on_LoginButton_pressed():
 # Called when register button is pressed
 func _on_RegisterButton_pressed():
 	get_tree().change_scene("res://RegisterScreen.tscn")
+	
