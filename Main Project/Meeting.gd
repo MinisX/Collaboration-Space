9e4291bc757fa3c88399fd842202941ede0036c6
fsk <fsk>
2022-10-09 21:08:34 +0200

    Changed participant_name String to participant_data Dictionary

--- Main Project/Meeting.gd
+++ Main Project/Meeting.gd
@@ -1,6 +1,10 @@
 extends Node
 
-var participant_name: String = "Participant"
+# participant data
+var participant_data: Dictionary = {
+	name = "name",
+	color = Color(1.0, 1.0, 1.0, 1.0) 
+}
 
 func _ready() -> void:
 
