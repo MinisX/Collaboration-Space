2bbebe61a2b9218f1b24a434fa752f20bcb63169
ys <ys>
2022-10-09 19:49:38 +0200

    Added idle animation to the participant

--- Main Project/Participant.gd
+++ Main Project/Participant.gd
@@ -1,17 +1,22 @@
 extends KinematicBody2D
 
-export (float) var speed: float = 10000.0
+export (float) var speed: float = 200.0
 
 var velocity: Vector2 = Vector2(0.0, 0.0)
 
 onready var participant_name: Label = $Name
+onready var animation_player: AnimationPlayer = $Animation
+
 
 func _ready() -> void:
 	participant_name.text = "Participant"
 
-func _process(delta: float) -> void:
+
+func _process(_delta: float) -> void:
 	get_input()
-	velocity = move_and_slide(velocity*delta)
+	decide_animation()
+	velocity = move_and_slide(velocity)
+
 
 func get_input() -> void:
 	velocity = Vector2()
@@ -25,6 +30,29 @@ func get_input() -> void:
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
 
+
 func set_participant_name(new_name: String) -> void:
 	participant_name.text = new_name
+
+
+func decide_animation() -> void:
+	if velocity.x == 0 and velocity.y > 0:
+		animation_player.play("idle_s")
+	elif velocity.x > 0 and velocity.y > 0:
+		animation_player.play("idle_se")
+	elif velocity.x > 0 and velocity.y == 0:
+		animation_player.play("idle_e")
+	elif velocity.x > 0 and velocity.y < 0:
+		animation_player.play("idle_ne")
+	elif velocity.x == 0 and velocity.y < 0:
+		animation_player.play("idle_n")
+	elif velocity.x < 0 and velocity.y < 0:
+		animation_player.play("idle_nw")
+	elif velocity.x < 0 and velocity.y == 0:
+		animation_player.play("idle_w")
+	elif velocity.x < 0 and velocity.y > 0:
+		animation_player.play("idle_sw")
+
+
+
 
