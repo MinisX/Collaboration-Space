472296daef2c95cc0ed99a82c153cc899a679667
ys <ys>
2022-10-11 19:27:43 +0200

    Updated participant-data dictionary for avatar customization

--- Main Project/Meeting.gd
+++ Main Project/Meeting.gd
@@ -2,10 +2,16 @@ extends Node
 
 # participant data
 var participant_data: Dictionary = {
-	name = "name",
-	color = Color(1.0, 1.0, 1.0, 1.0) 
+	Name = "name",
+	Color = {
+		Hair = Color(1.0, 1.0, 1.0, 1.0),
+		Skin = Color(1.0, 1.0, 1.0, 1.0),
+		Eyes = Color(1.0, 1.0, 1.0, 1.0),
+		Shirt = Color(1.0, 1.0, 1.0, 1.0),
+		Pants = Color(1.0, 1.0, 1.0, 1.0),
+		Shoe = Color(1.0, 1.0, 1.0, 1.0)
+	}
 }
 
 func _ready() -> void:
-
 	pass
