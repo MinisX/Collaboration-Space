e237298ef4e468a9fd041e05a22051329242a1c8
ys <ys>
2022-10-13 20:24:10 +0200

    Moved avatar to new folder

--- Main Project/Customization/Avatar.gd
+++ Main Project/Customization/Avatar.gd
@@ -1,13 +1,17 @@
-extends Node2D
+extends Control
 
 
-onready var sprites: Node2D = $Sprites
+
+
+onready var sprites: Control = $Sprites
 onready var animation: AnimationPlayer = $Animation
 onready var button_group: ButtonGroup = null
-onready var color_picker: ColorPickerButton = $MarginContainer/VBoxContainer/ColorPicker
+onready var color_picker: ColorPicker = $ColorPicker
 onready var turn_avatar_slider: HSlider = $MarginContainer/VBoxContainer/TurnAvatarSlider
+onready var ok_button: Button = $Ok
+onready var name_input: LineEdit = $NameInput
 
-var active_part: String = "Hair"
+var active_part: String = "Skin"
 var active_color: Color = Color(1.0, 1.0, 1.0, 1.0)
 
 func _ready() -> void:
@@ -15,7 +19,12 @@ func _ready() -> void:
 	button_group.connect("pressed", self, "_on_avatar_part_selected")
 	color_picker.connect("color_changed", self, "_on_color_value_changed")
 	turn_avatar_slider.connect("value_changed", self, "_on_avatar_turned")
+	ok_button.connect("pressed", self, "_on_ok_pressed")
 #	color_picker.connect("popup_closed", self, "_on_color_selected")
+
+func _on_ok_pressed() -> void:
+	Meeting.participant_data["Name"] = name_input.text
+	get_tree().change_scene("res://Spaces/Default/Default.tscn")
 
 
 func _on_color_value_changed(color: Color) -> void:
