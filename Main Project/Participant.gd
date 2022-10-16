9e57a2b580d8010a25f072ccf8414f51d396477d
ys <ys>
2022-10-16 13:26:45 +0200

    Simplified animation logic and bug fixes

--- Main Project/Participant.gd
+++ Main Project/Participant.gd
@@ -1,121 +1,106 @@
 extends KinematicBody2D
 
+
 export (float) var speed: float = 200.0
 
-var velocity: Vector2 = Vector2(0.0, 0.0)
-
 onready var participant_name: Label = $Name
-onready var animation_player: AnimationPlayer = $Animation
-onready var walk_animation: AnimationPlayer = $WalkAnimation
-onready var idle_sprites: Node2D = $IdleSprites
-onready var walk_sprites: Node2D = $WalkSprites
-onready var is_moving: bool = false
+onready var animation_player: AnimationPlayer = $AnimationPlayer
+onready var sprites_m: Node2D = $SpritesM
+
+var velocity: Vector2 = Vector2(0.0, 0.0)
+var direction: Vector2 = Vector2(0.0, 1.0)
+var current_animation: String = "idle_s"
+
 
 func _ready() -> void:
 	participant_name.text = "Participant"
+
+
+func _process(_delta: float) -> void:
+	get_input()
+	decide_animation()
+	animation_player.play(current_animation)
+	velocity = move_and_slide(velocity)
+
 
 func init(participant_dictionary: Dictionary) -> void:
 	participant_name.text = participant_dictionary["Name"]
 	set_selected_color()
 
 func set_selected_color() -> void:
-	# idle
-	idle_sprites.get_node("Hair").modulate = Meeting.participant_data["Color"]["Hair"]
-	idle_sprites.get_node("Eyes").modulate = Meeting.participant_data["Color"]["Eyes"]
-	# skin
-	idle_sprites.get_node("Head").modulate = Meeting.participant_data["Color"]["Skin"]
-	idle_sprites.get_node("Hands").modulate = Meeting.participant_data["Color"]["Skin"]
-	# shirt
-	idle_sprites.get_node("Torso").modulate = Meeting.participant_data["Color"]["Shirt"]
-	idle_sprites.get_node("Arms").modulate = Meeting.participant_data["Color"]["Shirt"]
-	# 
-	idle_sprites.get_node("Legs").modulate = Meeting.participant_data["Color"]["Pants"]
-	idle_sprites.get_node("Feet").modulate = Meeting.participant_data["Color"]["Shoe"]
-	# walk
-	walk_sprites.get_node("Hair").modulate = Meeting.participant_data["Color"]["Hair"]
-	walk_sprites.get_node("Eyes").modulate = Meeting.participant_data["Color"]["Eyes"]
-	# skin
-	walk_sprites.get_node("Head").modulate = Meeting.participant_data["Color"]["Skin"]
-	walk_sprites.get_node("Hands").modulate = Meeting.participant_data["Color"]["Skin"]
-	# shirt
-	walk_sprites.get_node("Torso").modulate = Meeting.participant_data["Color"]["Shirt"]
-	walk_sprites.get_node("Arms").modulate = Meeting.participant_data["Color"]["Shirt"]
-	# 
-	walk_sprites.get_node("Legs").modulate = Meeting.participant_data["Color"]["Pants"]
-	walk_sprites.get_node("Feet").modulate = Meeting.participant_data["Color"]["Shoe"]
-
-
-func _process(_delta: float) -> void:
-	get_input()
-	decide_animation()
-	swap_sprites()
-	velocity = move_and_slide(velocity)
+	sprites_m.get_node("Hair").modulate = Meeting.participant_data["Color"]["Hair"]
+	sprites_m.get_node("Eyes").modulate = Meeting.participant_data["Color"]["Eyes"]
+	sprites_m.get_node("Skin").modulate = Meeting.participant_data["Color"]["Skin"]
+	sprites_m.get_node("Shirt").modulate = Meeting.participant_data["Color"]["Shirt"]
+	sprites_m.get_node("Pants").modulate = Meeting.participant_data["Color"]["Pants"]
+	sprites_m.get_node("Shoe").modulate = Meeting.participant_data["Color"]["Shoe"]
 
 
 func get_input() -> void:
+	if (Input.is_action_pressed("ui_right") or
+		Input.is_action_pressed("ui_left") or
+		Input.is_action_pressed("ui_down") or
+		Input.is_action_pressed("ui_up")
+	):
+		direction = Vector2(0.0, 0.0)
+	
 	velocity = Vector2(0.0, 0.0)
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
+		direction.x = 1
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
+		direction.x =-1
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
+		direction.y = 1
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
+		direction.y = -1
 	velocity = velocity.normalized() * speed
-
-func swap_sprites() -> void:
-	if velocity.length() != 0.0:
-		idle_sprites.hide()
-		walk_sprites.show()
-		is_moving = true
-	else:
-		idle_sprites.show()
-		walk_sprites.hide()
-		is_moving = false
 
 
 func decide_animation() -> void:
-	if velocity.x == 0 and velocity.y > 0:
-		if is_moving:
-			walk_animation.play("walk_s")
+	if direction.x == 0 and direction.y > 0:
+		if velocity.length() == 0:
+			current_animation = "idle_s"
 		else:
-			animation_player.play("idle_s")
-	elif velocity.x > 0 and velocity.y > 0:
-		if is_moving:
-			walk_animation.play("walk_se")
+			current_animation = "walk_s"
+	elif direction.x > 0 and direction.y > 0:
+		if velocity.length() == 0:
+			current_animation = "idle_se"
 		else:
-			animation_player.play("idle_se")
-	elif velocity.x > 0 and velocity.y == 0:
-		if is_moving:
-			walk_animation.play("walk_e")
+			current_animation = "walk_se"
+	elif direction.x > 0 and direction.y == 0:
+		if velocity.length() == 0:
+			current_animation = "idle_e"
 		else:
-			animation_player.play("idle_e")
-	elif velocity.x > 0 and velocity.y < 0:
-		if is_moving:
-			walk_animation.play("walk_ne")
+			current_animation = "walk_e"
+	elif direction.x > 0 and direction.y < 0:
+		if velocity.length() == 0:
+			current_animation = "idle_ne"
 		else:
-			animation_player.play("idle_ne")
-	elif velocity.x == 0 and velocity.y < 0:
-		if is_moving:
-			walk_animation.play("walk_n")
+			current_animation = "walk_ne"
+	elif direction.x == 0 and direction.y < 0:
+		if velocity.length() == 0:
+			current_animation = "idle_n"
 		else:
-			animation_player.play("idle_n")
-	elif velocity.x < 0 and velocity.y < 0:
-		if is_moving:
-			walk_animation.play("walk_nw")
+			current_animation = "walk_n"
+	elif direction.x < 0 and direction.y < 0:
+		if velocity.length() == 0:
+			current_animation = "idle_nw"
 		else:
-			animation_player.play("idle_nw")
-	elif velocity.x < 0 and velocity.y == 0:
-		if is_moving:
-			walk_animation.play("walk_w")
+			current_animation = "walk_nw"
+	elif direction.x < 0 and direction.y == 0:
+		if velocity.length() == 0:
+			current_animation = "idle_w"
 		else:
-			animation_player.play("idle_w")
-	elif velocity.x < 0 and velocity.y > 0:
-		if is_moving:
-			walk_animation.play("walk_sw")
+			current_animation = "walk_w"
+	elif direction.x < 0 and direction.y > 0:
+		if velocity.length() == 0:
+			current_animation = "idle_sw"
 		else:
-			animation_player.play("idle_sw")
+			current_animation = "walk_sw"
 
 
 
