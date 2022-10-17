e33794803e0a31d6837361237015e6c2b60b990d
Amai Rivas <amai@Amais-MacBook-Pro.local>
2022-10-17 12:40:16 +0200

    Added an extra door

--- Main Project/Spaces/Default/Door.gd
+++ Main Project/Spaces/Default/Door.gd
@@ -1,19 +1,29 @@
 extends StaticBody2D
 
-onready var animationPlayer = $AnimationPlayer
-onready var interactiveArea = $interactingArea
+onready var animationPlayer: AnimationPlayer = $AnimationPlayer
+onready var interactiveArea: Area2D = $interactingArea
+
 var open = false
+var can_interact = false
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _ready() -> void:
+	interactiveArea.connect("body_entered", self, "_on_interaction_range_entered")
+	interactiveArea.connect("body_exited", self, "_on_interaction_range_exited")
 
-func _process(delta):
-	if Input.is_action_pressed("ui_open") and open == false:
-		animationPlayer.play("open")
-		open = true
-	elif Input.is_action_pressed("ui_close") and open == true:
-		animationPlayer.play_backwards("open")
-		open = false
+func _process(delta: float) -> void:  
+	if can_interact:
+		if Input.is_action_pressed("ui_open") and open == false:
+			animationPlayer.play("open")
+			open = true
+		elif Input.is_action_pressed("ui_close") and open == true:
+			animationPlayer.play_backwards("open")
+			open = false
 		
+func _on_interaction_range_entered(body) -> void:
+	if body.name == "Participant":
+		can_interact = true
 
+func _on_interaction_range_exited(body) -> void:
+	if body.name == "Participant":
+		can_interact = false
