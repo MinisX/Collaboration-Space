edf5d551c84980fd70e5237f305d75af09a8cb48
AssylbekBugybay <92851598+AssylbekBugybay@users.noreply.github.com>
2022-10-19 17:12:58 +0200

    Hide button online participants

--- Main Project/Participants/ParticipantsUI.gd
+++ Main Project/Participants/ParticipantsUI.gd
@@ -1,5 +1,6 @@
 extends Control
 
+onready var participants_ok_button = $ParticipantsPanel/HideButton
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,9 +9,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	participants_ok_button.connect("pressed", self, "_on_participants_ok_button_pressed")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_participants_ok_button_pressed() -> void:
+	self.hide()
--- Main Project/Spaces/Default/Default.gd
+++ Main Project/Spaces/Default/Default.gd
@@ -5,15 +5,16 @@ onready var chat_button: Button = $CanvasLayer/ChatButton
 onready var chat_UI = $CanvasLayer/ChatUI
 onready var menu_UI = $CanvasLayer/MenuUI
 onready var participants_button = $CanvasLayer/ParticipantsButton
-onready var participants_ok_button = $CanvasLayer/ParticipantsUI/ParticipantsPanel/HideButton
+
 
 var menu_visibility: bool = false
+var participants_UI_visibility: bool = false
 
 func _ready() -> void:
 	#particpant.init(GlobalData.participant_data)
 	chat_button.connect("pressed", self, "_on_chat_button_pressed")
 	participants_button.connect("pressed", self, "_on_participants_button_pressed")
-	participants_ok_button.connect("Ok pressed", self, "_on_participants_ok_button_pressed")
+
 
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -28,7 +29,10 @@ func _on_chat_button_pressed() -> void:
 	chat_UI.show()
 
 func _on_participants_button_pressed() -> void:
-	$CanvasLayer/ParticipantsUI.show()
+	if participants_UI_visibility ==false:
+		$CanvasLayer/ParticipantsUI.show()
+		participants_UI_visibility =true
+	else:
+		$CanvasLayer/ParticipantsUI.hide()
+		participants_UI_visibility =false
 	
-func _on_participants_ok_button_pressed() -> void:
-	$CanvasLayer/ParticipantsUI.hide()
