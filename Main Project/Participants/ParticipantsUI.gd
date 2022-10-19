f4801b038a1adb7bc9eb332e78813e28b36f29cf
AssylbekBugybay <92851598+AssylbekBugybay@users.noreply.github.com>
2022-10-19 19:13:13 +0200

    List of online participant done

--- Main Project/Participants/ParticipantsUI.gd
+++ Main Project/Participants/ParticipantsUI.gd
@@ -1,16 +1,21 @@
 extends Control
 
 onready var participants_ok_button = $ParticipantsPanel/HideButton
-
+onready var participants_list_view: ItemList = $ParticipantsPanel/ItemList
+onready var participants: Array = Meeting.get_participant_list()
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
+# TODO: runned only once, need some implementation during realtime new participant enter
+#Probably signal from default
 func _ready():
 	participants_ok_button.connect("pressed", self, "_on_participants_ok_button_pressed")
-
+	participants.sort()
+	participants_list_view.clear()
+	participants_list_view.add_item(Meeting.get_participant_name() + " (You)")
+	for p in participants:
+		participants_list_view.add_item(p)
 
 func _on_participants_ok_button_pressed() -> void:
 	self.hide()
