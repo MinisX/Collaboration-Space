81ad6b45318f09eb6332821798d903e4b599604d
fsk <fsk>
2022-10-19 19:42:39 +0200

    Added HotKeys scene

---
+++ Main Project/Menu/HotKeys.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+onready var back_button: Button = $MarginContainer/VBoxContainer/BackButton
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	back_button.connect("pressed", self, "_on_back_pressed")
+
+
+func _on_back_pressed() -> void:
+	$".".hide()
