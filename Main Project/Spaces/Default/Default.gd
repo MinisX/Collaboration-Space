457e7aa6cd75e7ff8c8472ff056de7a413dbfb50
AssylbekBugybay <92851598+AssylbekBugybay@users.noreply.github.com>
2022-10-19 16:40:15 +0200

    Online Participants UI

--- Main Project/Spaces/Default/Default.gd
+++ Main Project/Spaces/Default/Default.gd
@@ -4,12 +4,16 @@ extends YSort
 onready var chat_button: Button = $CanvasLayer/ChatButton
 onready var chat_UI = $CanvasLayer/ChatUI
 onready var menu_UI = $CanvasLayer/MenuUI
+onready var participants_button = $CanvasLayer/ParticipantsButton
+onready var participants_ok_button = $CanvasLayer/ParticipantsUI/ParticipantsPanel/HideButton
 
 var menu_visibility: bool = false
 
 func _ready() -> void:
 	#particpant.init(GlobalData.participant_data)
 	chat_button.connect("pressed", self, "_on_chat_button_pressed")
+	participants_button.connect("pressed", self, "_on_participants_button_pressed")
+	participants_ok_button.connect("Ok pressed", self, "_on_participants_ok_button_pressed")
 
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -22,3 +26,9 @@ func _process(_delta):
 
 func _on_chat_button_pressed() -> void:
 	chat_UI.show()
+
+func _on_participants_button_pressed() -> void:
+	$CanvasLayer/ParticipantsUI.show()
+	
+func _on_participants_ok_button_pressed() -> void:
+	$CanvasLayer/ParticipantsUI.hide()
