b5387b86888e655496805231f20e2ef4ce48410b
MinisX <daniel.dedoukh@gmail.com>
2022-11-07 17:08:09 +0100

    small fix

--- Main Project/ChatProposal/ChatUI/Client.gd
+++ Main Project/ChatProposal/ChatUI/Client.gd
@@ -43,7 +43,7 @@ func _exit_tree():
 	
 # This has to be removed for server pck (This Error shouldn't cause game to crash)
 
-"""
+
 func send_to_server(id, room, state):
 	var to_send = "{\"type\": \"assign_room\", \"user_id\": \"" + id + "\", \"room\": \"" + room + "\", \"state\": \"" + state + "\"}"
 	_client.get_peer(1).put_packet(to_send.to_utf8())
@@ -55,4 +55,4 @@ func send_user_id():
 	var to_send = "{\"type\": \"register\", " + "\"user_id\": \"" + Firebase.user_info.id + "\", \"name\": \""+ Meeting.participant_data["Name"] + "\"}"
 	_client.get_peer(1).put_packet(to_send.to_utf8())
 	print("Client: send_user_id")
-"""
+
