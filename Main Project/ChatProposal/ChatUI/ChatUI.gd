d7c7677e78023a63f6c5258ec203fac947e0a062
fsk <fsk>
2022-11-11 11:50:07 +0100

    Changed sender name color

--- Main Project/ChatProposal/ChatUI/ChatUI.gd
+++ Main Project/ChatProposal/ChatUI/ChatUI.gd
@@ -34,10 +34,10 @@ func _on_data():
 				for document in json_messages.result:
 					if "sender_name" in document and "message" in document:
 						print("document: " ,document)
-						_chat_text.append_bbcode("[color=blue]" + document["sender_name"] + "[/color]: " + document["message"])
+						_chat_text.append_bbcode("[color=#cccccc]" + document["sender_name"] + "[/color]: " + document["message"])
 						_chat_text.newline()
 		elif received_data.result["type"] == "message":
-			_chat_text.append_bbcode("[color=blue]" + received_data.result["name"] + "[/color]: " + received_data.result["msg"])
+			_chat_text.append_bbcode("[color=#cccccc]" + received_data.result["name"] + "[/color]: " + received_data.result["msg"])
 			_chat_text.newline()
 	print("ChatUI: inside _on_data")
 	print(Client._client.get_peer(1).get_packet().get_string_from_utf8())
@@ -53,7 +53,7 @@ func _on_send() -> void:
 	var to_send = "{\"type\": \"message\", " + "\"msg\": \"" + _line_edit.text + "\"}"
 	Client._client.get_peer(1).put_packet(to_send.to_utf8())
 	# Add text to text of the chat
-	_chat_text.append_bbcode("[color=blue]" + Meeting.participant_data["Name"] + "[/color]: " +_line_edit.text)
+	_chat_text.append_bbcode("[color=#cccccc]" + Meeting.participant_data["Name"] + "[/color]: " +_line_edit.text)
 	_chat_text.newline()
 	print("ChatUI: inside _on_send called")
 	print(_line_edit.text)
